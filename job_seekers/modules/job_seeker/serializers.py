5b5d1c416dad721252a0b2157d67f808935fd70d
Mustafa Hassan <Mustafa Hassan>
2024-09-16 11:22:56 +0500

    updated job_seeker serializers

--- job_seekers/modules/job_seeker/serializers.py
+++ job_seekers/modules/job_seeker/serializers.py
@@ -28,3 +28,21 @@ class BasicProfileSerializer(serializers.ModelSerializer):
             "status",
         ]
 
+
+class ProfilePictureSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = JobSeeker
+        fields = ['profile_picture_url']
+
+    def validate(self, data):
+        """
+        Ensure that either 'profile_picture' or 'profile_picture_url' is provided, but not both.
+        """
+        profile_picture = data.get('profile_picture')
+        profile_picture_url = data.get('profile_picture_url')
+
+        if profile_picture and profile_picture_url:
+            raise serializers.ValidationError("Provide either a profile picture or a profile picture URL, not both.")
+        return data
+
+
